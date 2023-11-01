8c6ea5a0a2668f5e759d6aaf59a0da4c9db48788
Olatunde16 <olatundeolutunmibi@gmail.com>
2023-11-01 07:17:51 -0700

    Still Ongoing

--- main.c
+++ main.c
@@ -1,57 +1,62 @@
 #include "main.h"
 
-int main() {
-    char input[MAX_INPUT_LENGTH];
-    char *args[MAX_ARGS];
-    int num_args;
+/**
+ * main - This is the main function of the Simple Shell
+ * num_args: Used to Keep track of the number of arguments
+ * provided by the user.
+ *
+ * Return: 0
+ */
 
-    while (1) {
-        // Display prompt
-        printf("Simple-Shell (PID: %d)> ", getpid());
+int main(void) 
+{
+	char input[MAX_INPUT_LENGTH];
+	char *args[MAX_ARGS];
+	int num_args;
 
-        // Get user input
-        fgets(input, MAX_INPUT_LENGTH, stdin);
-
-        // Remove newline character
-        input[strlen(input) - 1] = '\0';
-
-        // Exit if user enters "exit"
-        if (strcmp(input, "exit") == 0) {
-            break;
-        }
-
-	// Tozkenize the input into arguments
-	char *token = strtok(input, " ");
-	num_args = 0;
-
-	while (token != NULL){
-		args[num_args++] = token;
-		token = strtok(NULL, " ");
+	while (1) 
+	{
+		printf("Simple-Shell (PID: %d)> ", getpid());
+		
+		fgets(input, MAX_INPUT_LENGTH, stdin);
+		
+		input[strlen(input) - 1] = '\0';
+		
+		if user enters "exit"
+		if (strcmp(input, "exit") == 0) 
+		{
+			break;
+		}
+		
+		char *token = strtok(input, " ");
+		num_args = 0;
+		
+		while (token != NULL)
+		{
+			args[num_args++] = token;
+			token = strtok(NULL, " ");
+		}
+		
+		args[num_args] = NULL;
+		pid_t pid = fork();
+		
+		if (pid == 0) 
+		{
+			execvp(args[0], args);
+			perror("Command not found");
+			exit(1);
+		} 
+		
+		else if (pid > 0) 
+		{
+			wait(NULL);
+		} 
+		
+		else 
+		{
+			perror("Fork failed");
+		}
 	}
-
-	// Add a NULL pointer to the end of the args array (execvp required)
-	args[num_args] = NULL;
-
-        // Fork a new process
-        pid_t pid = fork();
-
-        if (pid == 0) {
-            // Child process
-            // Execute the command
-            execvp(args[0], args);
-
-            // If execvp() fails, print an error message
-            perror("Command not found");
-            exit(1);
-        } else if (pid > 0) {
-            // Parent process
-            // Wait for the child process to complete
-            wait(NULL);
-        } else {
-            // Fork failed
-            perror("Fork failed");
-        }
-    }
-
-    return 0;
+	
+	return (0);
 }
