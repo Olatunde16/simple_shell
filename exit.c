c2caf29d2a52149dd4ab0d6faec85abd38b786d1
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-11-04 15:59:13 +0100

    Update exit.c

--- exit.c
+++ exit.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 int pid = my_fork();
-if(pid == -1){
+if(pid == -1)
+{
 	printf("failed\n"); // We failed - bail out.
 }
-else if(pid > 0){ // Let the parent wait
+else if(pid > 0)
+{ 
 	int status;
 	waitpid(pid, &status, 0);
 }
-else{ // child
+else
+{ 
 	execvp(command, argument);
 }
