56cbb759dce489c5fcab8ad762811e395ff2f9d8
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-11-04 15:53:58 +0100

    Update filename.c

--- filename.c
+++ filename.c
@@ -1,45 +1,54 @@
 #include "main.h"
 
-void execute_command(char *args[]) {
+void execute_command(char *args[]) 
+{
     pid_t pid = fork();
 
-    if (pid == 0) {
-        // Child process
+    if (pid == 0) 
+    {
         execvp(args[0], args);
         perror("Command not found");
         exit(1);
-    } else if (pid > 0) {
-        // Parent process
+    } 
+    
+    else if (pid > 0) 
+    {
         int status;
         wait(&status);
 
-        if (WIFEXITED(status)) {
+        if (WIFEXITED(status)) 
+        {
             printf("Child process exited with status %d\n", WEXITSTATUS(status));
         }
-    } else {
-        // Fork failed
+    } 
+    
+    else 
+    {
         perror("Fork failed");
     }
 }
 
-void execute_commands_from_file(const char *filename) {
+void execute_commands_from_file(const char *filename) 
+{
     FILE *file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL) 
+    {
         perror("Error opening file");
         return;
     }
 
     char line[MAX_INPUT_LENGTH];
 
-    while (fgets(line, sizeof(line), file) != NULL) {
-        // Remove newline character
+    while (fgets(line, sizeof(line), file) != NULL) 
+    {
         line[strlen(line) - 1] = '\0';
 
         char *args[MAX_ARGS];
         int num_args = 0;
         char *token = strtok(line, " ");
 
-        while (token != NULL) {
+        while (token != NULL) 
+        {
             args[num_args++] = token;
             token = strtok(NULL, " ");
         }
@@ -52,36 +61,41 @@ void execute_commands_from_file(const char *filename) {
     fclose(file);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc == 2) {
-        // If a filename is provided as a command-line argument
+int main(int argc, char *argv[]) 
+{
+    if (argc == 2) 
+    {
         execute_commands_from_file(argv[1]);
-    } else if (argc > 2) {
+    } 
+    
+    else if (argc > 2) 
+    {
         printf("Usage: %s [filename]\n", argv[0]);
         return 1;
-    } else {
+    } 
+    
+    else 
+    {
         char input[MAX_INPUT_LENGTH];
 
-        while (1) {
-            // Display prompt and wait for user input
+        while (1) 
+        {
             printf("simple_shell> ");
 
-            // Get user input
-            if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) {
-                // Handle Ctrl+D (end of file)
+            if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) 
+            {
                 printf("\nExiting...\n");
                 break;
             }
 
-            // Remove newline character
             input[strlen(input) - 1] = '\0';
 
-            // Execute the command
             char *args[MAX_ARGS];
             int num_args = 0;
             char *token = strtok(input, " ");
 
-            while (token != NULL) {
+            while (token != NULL) 
+            {
                 args[num_args++] = token;
                 token = strtok(NULL, " ");
             }
