2b5066abdebfb1f474c871f86a1129a8aa731c05
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-11-06 12:36:51 +0100

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -1,78 +1,99 @@
 #include "main.h"
 
-int execute_command(char *args[]) {
+int execute_command(char *args[]) 
+{
     pid_t pid = fork();
-
-    if (pid == 0) {
+    
+    if (pid == 0) 
+    {
         execvp(args[0], args);
         perror("Command not found");
         exit(1);
-    } else if (pid > 0) {
+    } 
+    
+    else if (pid > 0) 
+    {
         int status;
         wait(&status);
         return WEXITSTATUS(status);
-    } else {
+    } 
+    
+    else 
+    {
         perror("Fork failed");
-        return -1;
+        return (-1);
     }
 }
 
-int main() {
+int main(void) 
+{
     char input[MAX_INPUT_LENGTH];
     char *args[MAX_ARGS];
     int num_args;
-
-    while (1) {
+    
+    while (1) 
+    {
         printf("SimpleShell (PID: %d)> ", getpid());
-
         fgets(input, MAX_INPUT_LENGTH, stdin);
-
         input[strlen(input) - 1] = '\0';
-
-        if (strcmp(input, "exit") == 0) {
-            break;
-        }
-
+        
+        if (strcmp(input, "exit") == 0) 
+                    break;
+        
         char *token = strtok(input, " ");
         num_args = 0;
-
-        while (token != NULL) {
+        
+        while (token != NULL) 
+        {
             args[num_args++] = token;
             token = strtok(NULL, " ");
         }
-
+        
         args[num_args] = NULL;
-
         int result = execute_command(args);
-
-        if (result == 0 && strstr(input, "&&") != NULL) {
+        
+        if (result == 0 && strstr(input, "&&") != NULL) 
+        {
             token = strtok(input, "&&");
             token = strtok(NULL, "&&");
-            if (token != NULL) {
+            
+            if (token != NULL) 
+            {
                 num_args = 0;
                 token = strtok(token, " ");
-                while (token != NULL) {
+                
+                while (token != NULL) 
+                {
                     args[num_args++] = token;
                     token = strtok(NULL, " ");
                 }
+                
                 args[num_args] = NULL;
                 result = execute_command(args);
             }
-        } else if (result != 0 && strstr(input, "||") != NULL) {
+        } 
+        
+        else if (result != 0 && strstr(input, "||") != NULL) 
+        {
             token = strtok(input, "||");
             token = strtok(NULL, "||");
-            if (token != NULL) {
+            
+            if (token != NULL) 
+            {
                 num_args = 0;
                 token = strtok(token, " ");
-                while (token != NULL) {
+                
+                while (token != NULL) 
+                {
                     args[num_args++] = token;
                     token = strtok(NULL, " ");
                 }
+                
                 args[num_args] = NULL;
                 result = execute_command(args);
             }
         }
     }
 
-    return 0;
+    return (0);
 }
