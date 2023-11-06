8c9e92c6b7f0f0b90e550a5cdf64f762e6140951
Olatunde16 <olatundeolutunmibi@gmail.com>
2023-11-06 04:01:21 -0800

    Still Ongoing

--- handlers.c
+++ handlers.c
@@ -1,121 +1,117 @@
 #include "main.h"
 
-/*
+/**
  * execute_command - Executes a command in a child process.
  *
- * This function creates a child process using fork() and attempts 
- * to execute the
- * command specified in the args array. If the command is found and 
- * executed successfully,
- * the parent process waits for the child process to complete and 
- * retrieves its exit status.
- * If the command execution fails, an error message is printed.
+ * This function creates a child process using fork() and attempts
+ * to execute the command specified in the args array. If the 
+ * command is found and executed successfully,the parent process
+ * waits for the child process to complete and retrieves its exit 
+ * status.If the command execution fails, an error message is printed.
  *
- * Args:
- *   args: An array of strings where the first element is the command 
- * and the subsequent
- *         elements are its arguments. The last element should be NULL as 
- * required by execvp().
+ * args: An array of strings where the first element is the command 
+ * and the subsequent elements are its arguments. The last element 
+ * should be NULL as required by execvp().
  *
- * Return Value:
- *   - Returns the exit status of the child process if the command was 
- * executed successfully.
- *   - Returns -1 if fork() fails.
+ * Return:
+ * - Returns the exit status of the child process if the command was 
+ *   executed successfully.
+ * - Returns -1 if fork() fails.
  */
+
 int execute_command(char *args[]) 
 {
-    pid_t pid = fork();
-    
-    if (pid == 0) 
-    {
-        execvp(args[0], args);
-        perror("Command not found");
-        exit(1);
-    } 
-    
-    else if (pid > 0) 
-    {
-        int status;
-        wait(&status);
-        return WEXITSTATUS(status);
-    } 
-    
-    else 
-    {
-        perror("Fork failed");
-        return (-1);
-    }
+	pid_t pid = fork();
+	
+	if (pid == 0) 
+	{
+		execvp(args[0], args);
+		perror("Command not found");
+		exit(1);
+	}
+	
+	else if (pid > 0) 
+	{
+		int status;
+		wait(&status);
+		return WEXITSTATUS(status);
+	} 
+	
+	else 
+	{
+		perror("Fork failed");
+		return (-1);
+	}
 }
 
 int main(void) 
 {
-    char input[MAX_INPUT_LENGTH];
-    char *args[MAX_ARGS];
-    int num_args;
-    
-    while (1) 
-    {
-        printf("SimpleShell (PID: %d)> ", getpid());
-        fgets(input, MAX_INPUT_LENGTH, stdin);
-        input[strlen(input) - 1] = '\0';
-        
-        if (strcmp(input, "exit") == 0) 
-                    break;
-        
-        char *token = strtok(input, " ");
-        num_args = 0;
-        
-        while (token != NULL) 
-        {
-            args[num_args++] = token;
-            token = strtok(NULL, " ");
-        }
-        
-        args[num_args] = NULL;
-        int result = execute_command(args);
-        
-        if (result == 0 && strstr(input, "&&") != NULL) 
-        {
-            token = strtok(input, "&&");
-            token = strtok(NULL, "&&");
-            
-            if (token != NULL) 
-            {
-                num_args = 0;
-                token = strtok(token, " ");
-                
-                while (token != NULL) 
-                {
-                    args[num_args++] = token;
-                    token = strtok(NULL, " ");
-                }
-                
-                args[num_args] = NULL;
-                result = execute_command(args);
-            }
-        } 
-        
-        else if (result != 0 && strstr(input, "||") != NULL) 
-        {
-            token = strtok(input, "||");
-            token = strtok(NULL, "||");
-            
-            if (token != NULL) 
-            {
-                num_args = 0;
-                token = strtok(token, " ");
-                
-                while (token != NULL) 
-                {
-                    args[num_args++] = token;
-                    token = strtok(NULL, " ");
-                }
-                
-                args[num_args] = NULL;
-                result = execute_command(args);
-            }
-        }
-    }
-
-    return (0);
+	char input[MAX_INPUT_LENGTH];
+	char *args[MAX_ARGS];
+	int num_args;
+	
+	while (1) 
+	{
+		printf("SimpleShell (PID: %d)> ", getpid());
+		fgets(input, MAX_INPUT_LENGTH, stdin);
+		input[strlen(input) - 1] = '\0';
+		
+		if (strcmp(input, "exit") == 0) 
+			break;
+		
+		char *token = strtok(input, " ");
+		num_args = 0;
+		
+		while (token != NULL) 
+		{
+			args[num_args++] = token;
+			token = strtok(NULL, " ");
+		}
+		
+		args[num_args] = NULL;
+		int result = execute_command(args);
+		
+		if (result == 0 && strstr(input, "&&") != NULL) 
+		{
+			token = strtok(input, "&&");
+			token = strtok(NULL, "&&");
+			
+			if (token != NULL) 
+			{
+				num_args = 0;
+				token = strtok(token, " ");
+				
+				while (token != NULL) 
+				{
+					args[num_args++] = token;
+					token = strtok(NULL, " ");
+				}
+				
+				args[num_args] = NULL;
+				result = execute_command(args);
+			}
+		}
+		
+		else if (result != 0 && strstr(input, "||") != NULL)
+		{
+			token = strtok(input, "||");
+			token = strtok(NULL, "||");
+			
+			if (token != NULL) 
+			{
+				num_args = 0;
+				token = strtok(token, " ");
+				
+				while (token != NULL) 
+				{
+					args[num_args++] = token;
+					token = strtok(NULL, " ");
+				}
+				
+				args[num_args] = NULL;
+				result = execute_command(args);
+			}
+		}
+	}
+	return (0);
 }
