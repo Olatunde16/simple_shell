074740a4d239f6530c8017bd1d1469c7541c5b67
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-11-16 08:58:07 +0100

    Update main.c

--- main.c
+++ main.c
@@ -22,47 +22,41 @@ int main(void)
 	char input[MAX_INPUT_LENGTH];
 	char *args[MAX_ARGS];
 	int num_args;	
-
-while (1) 
-{
-	printf("Simple-Shell (PID: %d)> ", getpid());
-	fgets(input, MAX_INPUT_LENGTH, stdin);
-	input[strlen(input) - 1] = '\0';
 	
-	if (strcmp(input, "exit") == 0) 
-	
-		break;
-	
-	char *token = strtok(input, " ");
-	
-	num_args = 0;
-	
-	while (token != NULL) 
+	while (1) 
 	{
-		args[num_args++] = token;
-		token = strtok(NULL, " ");
-	}
-	
-	args[num_args] = NULL;
-	pid_t pid = fork();
-	
-	if (pid == 0) 
-	{
-		execvp(args[0], args);
-		perror("Command not found");
-		exit(1);
-	} 
-	
-	else if (pid > 0) 
-	{
-		wait(NULL);
-	}
+		printf("Simple-Shell (PID: %d)> ", getpid());
+		fgets(input, MAX_INPUT_LENGTH, stdin);
+		input[strlen(input) - 1] = '\0';
 		
-	else 
-	{
-		perror("Fork failed");
+		if (strcmp(input, "exit") == 0) 
+			break;
+		
+		char *token = strtok(input, " ");
+		num_args = 0;
+		
+		while (token != NULL) 
+		{
+			args[num_args++] = token;
+			token = strtok(NULL, " ");
+		}
+		
+		args[num_args] = NULL;
+		pid_t pid = fork();
+		
+		if (pid == 0) 
+		{
+			execvp(args[0], args);
+			perror("Command not found");
+			exit(1);
+		} 
+		
+		else if (pid > 0) 
+			wait(NULL);
+		
+		else 
+			perror("Fork failed");
 	}
-}
 	
 	return (0);
-	}
+}
