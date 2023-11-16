c2c2dcd5ddee77382872b6cbd2e20c4c635798df
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-11-16 08:18:11 +0100

    Update filename.c

--- filename.c
+++ filename.c
@@ -14,32 +14,28 @@
 void execute_command(char *args[]) 
 {
 	pid_t pid = fork();
-
-    if (pid == 0) 
-    {
-        execvp(args[0], args);
-        perror("Command not found");
-        exit(1);
-    } 
-    
-    else if (pid > 0) 
-    {
-        int status;
-        wait(&status);
-
-        if (WIFEXITED(status)) 
-        {
-            printf("Child process exited with status %d\n", WEXITSTATUS(status));
-        }
-    } 
-    
-    else 
-    {
-        perror("Fork failed");
-    }
+	
+	if (pid == 0) 
+	{
+		execvp(args[0], args);
+		perror("Command not found");
+		exit(1);
+	} 
+	
+	else if (pid > 0) 
+	{
+		int status;
+		wait(&status);
+		
+		if (WIFEXITED(status)) 
+			printf("Child process exited with status %d\n", WEXITSTATUS(status));
+	} 
+	
+	else 
+		perror("Fork failed");
 }
 
-/*
+/**
  * execute_commands_from_file - Executes commands from a file.
  *
  * This function takes a filename as input, opens the file, and reads lines
@@ -53,35 +49,36 @@ void execute_command(char *args[])
 
 void execute_commands_from_file(const char *filename) 
 {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) 
-    {
-        perror("Error opening file");
-        return;
-    }
-
-    char line[MAX_INPUT_LENGTH];
-
-    while (fgets(line, sizeof(line), file) != NULL) 
-    {
-        line[strlen(line) - 1] = '\0';
-
-        char *args[MAX_ARGS];
-        int num_args = 0;
-        char *token = strtok(line, " ");
-
-        while (token != NULL) 
-        {
-            args[num_args++] = token;
-            token = strtok(NULL, " ");
-        }
-
-        args[num_args] = NULL;
-
-        execute_command(args);
-    }
-
-    fclose(file);
+	FILE *file = fopen(filename, "r");
+	
+	if (file == NULL) 
+	{
+		perror("Error opening file");
+		return;
+	}
+	
+	char line[MAX_INPUT_LENGTH];
+	
+	while (fgets(line, sizeof(line), file) != NULL) 
+	{
+		line[strlen(line) - 1] = '\0';
+		
+		char *args[MAX_ARGS];
+		int num_args = 0;
+		char *token = strtok(line, " ");
+		
+		while (token != NULL) 
+		{
+			args[num_args++] = token;
+			token = strtok(NULL, " ");
+		}
+		
+		args[num_args] = NULL;
+		
+		execute_command(args);
+	}
+	
+	fclose(file);
 }
 
 /**
@@ -100,48 +97,46 @@ void execute_commands_from_file(const char *filename)
 
 int main(int argc, char *argv[]) 
 {
-    if (argc == 2) 
-    {
-        execute_commands_from_file(argv[1]);
-    } 
-    
-    else if (argc > 2) 
-    {
-        printf("Usage: %s [filename]\n", argv[0]);
-        return 1;
-    } 
-    
-    else 
-    {
-        char input[MAX_INPUT_LENGTH];
-
-        while (1) 
-        {
-            printf("simple_shell> ");
-
-            if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) 
-            {
-                printf("\nExiting...\n");
-                break;
-            }
-
-            input[strlen(input) - 1] = '\0';
-
-            char *args[MAX_ARGS];
-            int num_args = 0;
-            char *token = strtok(input, " ");
-
-            while (token != NULL) 
-            {
-                args[num_args++] = token;
-                token = strtok(NULL, " ");
-            }
-
-            args[num_args] = NULL;
-
-            execute_command(args);
-        }
-    }
-
-    return (0);
+	if (argc == 2) 
+		execute_commands_from_file(argv[1]);
+	
+	else if (argc > 2) 
+	{
+		printf("Usage: %s [filename]\n", argv[0]);
+		return (1);
+	} 
+	
+	else 
+	{
+		char input[MAX_INPUT_LENGTH];
+		
+		while (1) 
+		{
+			printf("simple_shell> ");
+			
+			if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) 
+			{
+				printf("\nExiting...\n");
+				break;
+			}
+			
+			input[strlen(input) - 1] = '\0';
+			
+			char *args[MAX_ARGS];
+			int num_args = 0;
+			char *token = strtok(input, " ");
+			
+			while (token != NULL) 
+			{
+				args[num_args++] = token;
+				token = strtok(NULL, " ");
+			}
+			
+			args[num_args] = NULL;
+			
+			execute_command(args);
+		}
+	}
+	
+	return (0);
 }
