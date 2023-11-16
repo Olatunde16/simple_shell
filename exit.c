4354e1ec2c411b6fa34f51e73978ba19b8c61d16
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-11-16 17:20:52 +0100

    Update exit.c

--- exit.c
+++ exit.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 int pid = my_fork();
+
 if(pid == -1)
-{
 	printf("failed\n");
-}
+
 else if(pid > 0)
 { 
 	int status;
 	waitpid(pid, &status, 0);
 }
+	
 else
-{ 
 	execvp(command, argument);
-}
